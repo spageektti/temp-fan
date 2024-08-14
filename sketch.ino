1f49acc01d3e122bbe4012b82fcb9bfe2140cdbd
spageektti <git@spageektti.cc>
2024-08-14 16:37:18 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,20 +1,16 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
 #include <Adafruit_NeoPixel.h>
 
-#define ONE_WIRE_BUS 26   // data pin temp sensor
+#define THERMISTOR_PIN 26  // GPIO pin for thermistor (ADC)
 #define RELAY_PIN 2       
 #define BUZZER_PIN 3      
 #define NEOPIXEL_PIN 0    // DIN
 #define NUMPIXELS 1       // bin ships only one
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
 Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
-  sensors.begin();
+const float BETA = 3950; // coefficient of the thermistor from wokwi docs
 
+void setup() {
   pixels.begin();
 
   pinMode(RELAY_PIN, OUTPUT);
@@ -27,12 +23,13 @@ void setup() {
 }
 
 void loop() {
-  sensors.requestTemperatures();
-  
-  float temperatureC = sensors.getTempCByIndex(0);
+  int analogValue = analogRead(THERMISTOR_PIN);
+
+  // like in wokwi docs
+  float celsius = 1 / (log(1 / (4095.0 / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
 
   Serial1.print("Temperature: ");
-  Serial1.print(temperatureC);
+  Serial1.print(celsius);
   Serial1.println(" C");
 
   delay(1000);
